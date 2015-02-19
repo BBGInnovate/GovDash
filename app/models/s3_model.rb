36c60ad91e70b0c7911ef5fb560b9f90f4f1581f
liwenl <lliu@metrostarsystems.com>
2015-02-19 09:40:48 -0500

    rm activerecord4-redshift-adapter

--- app/models/s3_model.rb
+++ app/models/s3_model.rb
@@ -1,50 +1,35 @@
+require 'aws-sdk'
+
+=begin
+s3 = S3Model.client
+resp = s3.list_buckets
+resp.buckets.map(&:name)
+
+# list the first two objects in a bucket
+# object.key is the file_path
+bucket = 'uber-dashboard-socialscraper'
+resp = s3.list_objects(bucket: bucket, max_keys: 12)
+resp.contents.each do |object|
+  puts "#{object.key} => #{object.etag}"
+end
+
+=end
+
 class S3Model < Object
   attr_accessor :file_path_name
-  # file_name = "facebook/voiceofamerica/insights/20140610.json"
-  # no leading "/"
   def initialize
-    connection!
-  end
-  
-  def connection!
-    AWS::S3::Base.establish_connection!(
-      :access_key_id     => S3.config[:s3_credentials][:access_key_id],
-      :secret_access_key => S3.config[:s3_credentials][:secret_access_key]
+    Aws.config[:region] = 'us-east-1'
+    Aws::S3::Client.new(
+      access_key_id: config[:s3_credentials][:access_key_id],
+      secret_access_key: config[:s3_credentials][:secret_access_key]
     )
-  end
-  
-  def bucket
-    @bucket ||= S3.config[:bucket]
   end
   
   def store(file_path, content)
-    begin
-    AWS::S3::S3Object.store(
-      file_path,
-      content,
-      bucket,
-      :content_type => 'text/json',:access => :public_read
-    )
-    rescue Exception=> e
-      Account.logger.info "#{e.message}"
-      Account.logger.info "#{e.backtrace}"
-    end
-    
-    puts "#{file_path} Uploaded!"
+    s3 = Aws::S3.new
+    s3.buckets[bucket].objects[file_path].write(content)
+  end
 
-  end
-  
-  def get_url(file_name)
-    file_name = file_name.sub(/^\//, '')
-    obj=AWS::S3::S3Object.find file_name, bucket
-    obj.url 
-  end
-  
-  def json_obj file_name
-    str = AWS::S3::S3Object.value file_name, bucket
-    json_obj = JSON.parse str
-  end
-  
   def download_insights(account)
     path = account.s3_filepath(date) + "insights.json"
     puts "Download from S3 #{path}. All dates are end date"
@@ -121,8 +106,56 @@ class S3Model < Object
     show_raw ? results : merge_arrays(arrays)
   end
   
+  protected
   
-  private
+  def bucket
+    @bucket ||= config[:bucket]
+  end
+  
+  def json_obj file_path_name
+    begin
+      resp = s3.get_object(bucket: bucket, key: file_path_name)
+      JSON.parse resp.body.read
+    rescue
+     "{}"
+    end
+  end
+  
+  def config
+    unless @config
+      creds = "#{Rails.root}/config/s3.yml"
+      res = YAML.load_file(creds)
+      # Environment specific definitions override global definitions
+      res1  = symbolize_keys(res.merge(res.delete(Rails.env)))
+      # delete entries other then for current environment
+      envs = [:development, :qa, :test, :staging, :production]
+      envs.each{|e| res1.delete e}
+      access_key_id = res1.delete(:access_key_id)
+      secret_access_key = res1.delete(:secret_access_key)
+      res1[:s3_credentials]={:access_key_id=>access_key_id, :secret_access_key=>secret_access_key}
+      @config = symbolize_keys res1
+    end
+    @config
+  end
+  
+  def symbolize_keys(hash)
+    hash.inject({}){|res, (key, val)|
+      nkey = case key
+        when String
+        key.to_sym
+      else
+        key
+      end
+      nval = case val
+        when Hash, Array
+        symbolize_keys(val)
+      else
+        val
+      end
+      res[nkey] = nval
+      res
+    }
+  end
   
   def convert(content)
     content = content.to_a
@@ -135,6 +168,38 @@ class S3Model < Object
     content
   end
   
+end
+=begin
+  def connection!
+    AWS::S3::Base.establish_connection!(
+      :access_key_id     => S3.config[:s3_credentials][:access_key_id],
+      :secret_access_key => S3.config[:s3_credentials][:secret_access_key]
+    )
+  end
+  
+  def get_url(file_name)
+    file_name = file_name.sub(/^\//, '')
+    obj=Aws::S3::S3Object.find file_name, bucket
+    obj.url 
+  end
+  
+  
+  def store(file_path, content)
+    begin
+    AWS::S3::S3Object.store(
+      file_path,
+      content,
+      bucket,
+      :content_type => 'text/json',:access => :public_read
+    )
+    rescue Exception=> e
+      Account.logger.info "#{e.message}"
+      Account.logger.info "#{e.backtrace}"
+    end
+    
+    puts "#{file_path} Uploaded!"
+
+  end
 end
 
 

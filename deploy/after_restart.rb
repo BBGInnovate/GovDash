36349cb553d647a6db6867a559ac6206035d03df
liwenl <lliu@metrostarsystems.com>
2015-02-24 08:14:03 -0500

    correct typo

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -2,6 +2,6 @@ rails_env = new_resource.environment["RAILS_ENV"]
 Chef::Log.info("Start deplayed_job for RAILS_ENV=#{rails_env}...")
 execute "deplayed_job start" do
   cwd release_path
-  command "bin/deplayed_job restart"
+  command "bin/delayed_job restart"
   environment "RAILS_ENV" => rails_env
 end

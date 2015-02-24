5a55a4c8c18422a10dbdc44b7537cd2f1d7928ef
liwenl <lliu@metrostarsystems.com>
2015-02-24 18:07:45 -0500

    modified

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -7,10 +7,25 @@ links << "rm config/s3.yml;ln -s ../shared/config/s3.yml config/s3.yml;"
 links << "rm  config/sitecatalyst.yml;ln -s ../shared/config/sitecatalyst.yml config/sitecatalyst.yml;"
 links << "rm config/database.yml;ln -s ../shared/config/database.yml config/database.yml;"
 Chef::Log.info("Start deplayed_job for RAILS_ENV=#{rails_env}...")
+=begin
 execute "deplayed_job start" do
   cwd release_path
   command "bin/delayed_job restart"
   environment "RAILS_ENV" => rails_env
   action :nothing
 end
+=end
+
+Chef::Log.info("Create sym links")
+run <<-END
+  cwd release_path
+  ln -s ../shared/config/email.yml config/email.yml;
+  ln -s ../shared/config/facebook.yml config/facebook.yml;
+  ln -s ../shared/config/rabbit.yml config/rabbit.yml;
+  ln -s ../shared/config/twitter.yml config/twitter.yml;
+  ln -s ../shared/config/s3.yml config/s3.yml;
+  ln -s ../shared/config/sitecatalyst.yml config/sitecatalyst.yml;
+  ln -s ../shared/config/database.yml config/database.yml;
+  bin/delayed_job restart
+END
 

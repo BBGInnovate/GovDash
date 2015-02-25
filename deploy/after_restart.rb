14471a14c2d0421b6a928555ea825ef9de2907cf
liwenl <lliu@metrostarsystems.com>
2015-02-24 20:25:08 -0500

    shared_path

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -17,7 +17,7 @@ end
 =end
 
 current_path = "#{release_path}/current"
-shared_path = "#{release_path}/shared"
+shared_path = "#{release_path}/../../shared"
   
 Chef::Log.info("Create sym links")
 run <<-END

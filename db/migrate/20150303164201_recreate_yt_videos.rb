94dce2d8a9d3023dfea900b5f933c163106774f0
liwenl <lliu@metrostarsystems.com>
2015-03-04 09:09:47 -0500

    recreate yt_videos table

---
+++ db/migrate/20150303164201_recreate_yt_videos.rb
@@ -0,0 +1,27 @@
+class RecreateYtVideos < ActiveRecord::Migration
+  def up
+    if ActiveRecord::Base.connection.table_exists? 'yt_videos'
+      drop_table :yt_videos
+    end
+    create_table :yt_videos do |t|
+      t.integer :yt_channel_id
+      t.string :video_id, :limit=>40
+      t.integer :likes
+      t.integer :comments
+      t.integer :favorites
+      t.datetime :published_at
+      t.timestamps
+    end
+    add_index :yt_videos, :yt_channel_id
+    add_index :yt_videos, :video_id, :unique=>true
+    add_index :yt_videos, :published_at
+  end
+  
+  def down
+    remove_index :yt_videos, :yt_channel_id
+    remove_index :yt_videos, :video_id
+    remove_index :yt_videos, :published_at
+    drop_table :yt_videos
+  end
+  
+end

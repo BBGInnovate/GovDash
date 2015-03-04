81c0c318a7783dc188987b9a6d9dceab3f750c74
liwenl <lliu@metrostarsystems.com>
2015-03-04 16:09:18 -0500

    add rescue

--- db/migrate/20150303174103_rename_services_to_subgroups.rb
+++ db/migrate/20150303174103_rename_services_to_subgroups.rb
@@ -1,9 +1,15 @@
 class RenameServicesToSubgroups < ActiveRecord::Migration
   def up
+    begin
   	rename_table :services, :subgroups
   	remove_column :subgroups, :network_id
+    rescue
+    end
   end
   def down
+    begin
   	rename_table :subgroups, :services
+    rescue
+    end
   end
 end

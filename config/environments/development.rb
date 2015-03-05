7c7982de2bfcf07162d34f3c7b38298aa3ec8d7f
liwenl <lliu@metrostarsystems.com>
2015-03-05 13:17:48 -0500

    remove DelayedJobWorkers

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,6 +33,5 @@ Radd::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-  
-  DelayedJobWorkers = 1
+
 end

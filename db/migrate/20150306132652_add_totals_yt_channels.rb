d5496192013aae0e763e9fcf40619b6d74ec1132
liwenl <lliu@metrostarsystems.com>
2015-03-06 10:49:19 -0500

    renamed column names

--- db/migrate/20150306132652_add_totals_yt_channels.rb
+++ db/migrate/20150306132652_add_totals_yt_channels.rb
@@ -1,8 +1,8 @@
 class AddTotalsYtChannels < ActiveRecord::Migration
   def change
-    add_column :yt_channels, :total_views, :integer, after: :subscribers, default: 0
-    add_column :yt_channels, :total_likes, :integer, after: :subscribers, default: 0
-    add_column :yt_channels, :total_favorites, :integer, after: :subscribers, default: 0
-    add_column :yt_channels, :total_comments, :integer, after: :subscribers, default: 0
+    add_column :yt_channels, :video_views, :integer, after: :subscribers, default: 0
+    add_column :yt_channels, :video_likes, :integer, after: :subscribers, default: 0
+    add_column :yt_channels, :video_favorites, :integer, after: :subscribers, default: 0
+    add_column :yt_channels, :video_comments, :integer, after: :subscribers, default: 0
   end
 end

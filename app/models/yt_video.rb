c95a520cc491862a24746cfe3c3d225eb7a1c3af
liwenl <lliu@metrostarsystems.com>
2015-03-06 10:15:20 -0500

    yt_channel_for

--- app/models/yt_video.rb
+++ app/models/yt_video.rb
@@ -11,8 +11,11 @@ class YtVideo < ActiveRecord::Base
   # to clear cache of method create_or_update
   handle_asynchronously :sync_redshift, :run_at => Proc.new { 5.seconds.from_now }
   
-  def yt_channel
-    YtChannel.order("id desc").find_by account_id: self.account_id
+  def yt_channel_for date=Time.zone.now
+    date = date.middle_of_day.to_s(:db)
+    YtChannel.
+      where("account_id=#{self.account_id} AND published_at = #{date}").
+      first
   end
-  
+
 end

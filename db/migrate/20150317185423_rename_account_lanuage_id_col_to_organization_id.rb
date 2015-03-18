98b2527ad98ab239bf1d7ce11363478ebf4bc6f6
liwenl <lliu@metrostarsystems.com>
2015-03-18 13:56:25 -0400

    add down

--- db/migrate/20150317185423_rename_account_lanuage_id_col_to_organization_id.rb
+++ db/migrate/20150317185423_rename_account_lanuage_id_col_to_organization_id.rb
@@ -2,4 +2,9 @@ class RenameAccountLanuageIdColToOrganizationId < ActiveRecord::Migration
   def self.up
     rename_column :accounts, :language_id, :organization_id
   end
+  
+  def self.down
+    rename_column :accounts, :organization_id, :language_id
+  end
+  
 end

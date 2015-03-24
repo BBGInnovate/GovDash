baaf555c4c2160e12bac18bae8ea7b239a36d2f9
liwenl <lliu@metrostarsystems.com>
2015-03-24 13:58:04 -0400

    disabled

--- app/models/account_observer.rb
+++ app/models/account_observer.rb
@@ -1,17 +1,18 @@
 # observe model related events
 class AccountObserver < ActiveRecord::Observer
   def after_create(account)
-    if account.media_type_name == 'FacebookAccount'
+    # not in use
+    if account.media_type_name == '__FacebookAccount'
       conf = YAML::load_file(File.join(Rails.root.to_s, 'config/facebook.yml'))
       ['development','production'].each do |env|
         uri = URI.parse conf[env]['canvas_url']
-        rec = ApiToken.find_or_create_by account_id: account.id, canvas_url: "#{uri.host}"
+        rec = AppToken.find_or_create_by account_id: account.id, canvas_url: "#{uri.host}"
       end
     end
   end
   
   def after_destroy(account)
-    account.api_tokens.each{|z| z.destroy}
+    # account.app_token.each{|z| z.destroy}
   end
   
 end

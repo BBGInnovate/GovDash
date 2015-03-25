b34c90de94418ceaca954f17e32263033aba7a3f
Eric Pugh <pugh.eric@gmail.com>
2015-03-25 11:30:32 -0400

    Add related groups_subgroups to api output

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,8 +1,11 @@
 class Group < ActiveRecord::Base
   belongs_to :organization
+  has_many :accounts
+  has_many :groups_subgroups, class_name: GroupsSubgroups
+  has_many :subgroups, :through => :groups_subgroups
+
   validates :name, length: { in: 2..40 }
-  
-  
+
   def self.filter_attr(attributes)
   
   end

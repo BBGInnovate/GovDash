e02edcae45b2d04e317f8a6051bd64de2d5ac368
liwenl <lliu@metrostarsystems.com>
2015-03-31 10:13:50 -0400

    add accounts_groups accounts_subgroups

---
+++ app/controllers/api/v2/accounts_groups_controller.rb
@@ -0,0 +1,7 @@
+class Api::V2::AccountsGroupsController < Api::V2::BaseController
+  # before_filter :authenticate_user!
+  #before_filter :is_analyst?
+
+  private
+
+end
---
+++ app/controllers/api/v2/accounts_subgroups_controller.rb
@@ -0,0 +1,7 @@
+class Api::V2::AccountsSubgroupsController < Api::V2::BaseController
+  # before_filter :authenticate_user!
+  #before_filter :is_analyst?
+
+  private
+
+end

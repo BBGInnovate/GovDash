a6d4b56b49acea6507c08549826c3c144c10f731
Hayat Noor <hnoor@bbg.gov>
2015-04-01 18:44:38 -0400

    fixed some account create / put related things, fixed account tables, working on prepopulated dropdown for group / subgroup

--- app/assets/javascripts/services/accountService.js
+++ app/assets/javascripts/services/accountService.js
@@ -2,9 +2,9 @@ angular.module('accountService', [])
     .factory('Accounts', function($location, $http, $q, $rootScope) {
        
         var account = {
-            
-            create: function(name, description, object_name, socialMediaAccount, organizationId, groupId, subgroupIds, languageId, regionIds, countryIds, accountTypeId, segmentIds) {
-                return $http.post('/api/accounts', {account: {name: name, description: description, object_name: object_name, media_type_name: socialMediaAccount, organization_id: organizationId, group_id: groupId, subgroup_ids: subgroupIds, language_id: languageId, region_ids: regionIds, country_ids: countryIds, account_type_id: accountTypeId, sc_segment_ids: segmentIds } })
+
+            create: function(name, description, object_name, socialMediaAccount, organizationId, groupIds, subgroupIds, languageIds, regionIds, countryIds, accountTypeId, segmentIds) {
+                return $http.post('/api/accounts', {account: {name: name, description: description, object_name: object_name, media_type_name: socialMediaAccount, group_ids: groupIds, subgroup_ids: subgroupIds, language_ids: languageIds, region_ids: regionIds, country_ids: countryIds, account_type_id: accountTypeId, sc_segment_ids: segmentIds } })
                 .then(function(response) {
                 //    console.log('Account Created!');
                 });
@@ -22,8 +22,8 @@ angular.module('accountService', [])
 					return account;
 				});
             },
-            update: function(accountId, name, description, object_name, socialMediaAccount, organizationId, groupId, subgroupIds, languageId, regionIds, countryIds, accountTypeId, segmentIds) {
-            	return $http.put('api/accounts/' + accountId, {account: { id: accountId, name: name, description: description, object_name: object_name, media_type_name: socialMediaAccount, organization_id: organizationId, group_id: groupId, subgroup_ids: subgroupIds, language_id: languageId, region_ids: regionIds, country_ids: countryIds, account_type_id: accountTypeId, sc_segment_ids: segmentIds } })
+            update: function(accountId, name, description, object_name, socialMediaAccount, organizationId, groupIds, subgroupIds, languageId, regionIds, countryIds, accountTypeId, segmentIds) {
+            	return $http.put('api/accounts/' + accountId, {account: { id: accountId, name: name, description: description, object_name: object_name, media_type_name: socialMediaAccount, group_ids: groupIds, subgroup_ids: subgroupIds, language_id: languageId, region_ids: regionIds, country_ids: countryIds, account_type_id: accountTypeId, sc_segment_ids: segmentIds } })
 				.then(function(response) {
                     //console.log('Account Updated!');
                 });

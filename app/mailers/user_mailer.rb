1e3d0cdb418b53a6086472d8243174bb5a34350f
liwenl <lliu@metrostarsystems.com>
2015-04-07 11:12:40 -0400

    add comment

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,7 @@
 class UserMailer < ActionMailer::Base
   default :from => "oddidev@bbg.gov"  
-  
+  # UserMailer.alarm_email(['liw@bbg.gov'], 'Testing AWS Email Service').deliver_now!
+ 
   def alarm_email(email, message)
       @message = message
       mail(to: email, subject: @message)

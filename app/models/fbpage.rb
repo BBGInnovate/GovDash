55f758d6f53ecdf0d5a0a06e6d46600ae245bb5b
liwenl <lliu@metrostarsystems.com>
2015-04-09 16:01:53 -0400

    copy_from_fb_pages

--- app/models/fbpage.rb
+++ app/models/fbpage.rb
@@ -7,18 +7,20 @@ class Fbpage < ActiveRecord::Base
     FacebookAccount.where("id>0").where("is_active=1").to_a.each do |acc|
       mypages = acc.fbpages
       acc.fb_pages.where("total_likes is not null").to_a.each do |fp_old|
-        d=fp_old.post_created_time
-        fp_new = mypages.where(post_created_time: (d.beginning_of_day..d.end_of_day)).
-          to_a.first
-        if !fp_new
-          fp_new = acc.fbpages.create(:post_created_time=>d.middle_of_day)
-        end
-        
-        if !fp_new.total_likes || (fp_new.total_likes < fp_old.total_likes)
-          puts " #{fp_new.total_likes} = #{fp_old.total_likes}"
-          fp_new.total_likes = fp_old.total_likes
-          fp_new.save
-          
+        begin
+          d=fp_old.post_created_time
+          fp_new = mypages.where(post_created_time: (d.beginning_of_day..d.end_of_day)).
+             to_a.first
+          if !fp_new
+            fp_new = acc.fbpages.create(:post_created_time=>d.middle_of_day)
+          end
+          if !fp_new.total_likes || (fp_new.total_likes < fp_old.total_likes)
+            puts " #{fp_new.total_likes} = #{fp_old.total_likes}"
+            fp_new.total_likes = fp_old.total_likes
+            fp_new.save
+          end
+        rescue exception=>ex
+          logger.error "  Fbpage.copy_from_fb_pages #{ex.message}"
         end
       end
     end

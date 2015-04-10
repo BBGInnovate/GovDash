5c01cc3652222c4d2f8113051da97e1345ca59c3
liwenl <lliu@metrostarsystems.com>
2015-04-10 16:00:57 -0400

    has_and_belongs_to_many

--- app/models/subgroup.rb
+++ app/models/subgroup.rb
@@ -3,7 +3,8 @@ class Subgroup < ActiveRecord::Base
   has_many :accounts
   has_many :groups_subgroups, class_name: GroupsSubgroups
   has_many :groups, :through => :groups_subgroups
-  
+  has_and_belongs_to_many :regions, :join_table => :subgroups_regions
+
   def self.find_me(name, group_name)
     nid = Group.find_by_name(group_name).id
     where("name='#{name}' AND group_id=#{nid}").first

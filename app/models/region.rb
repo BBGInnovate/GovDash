9f1278127e34d2e529fb8f25b388656b585f5abc
liwenl <lliu@metrostarsystems.com>
2015-04-10 16:00:57 -0400

    has_and_belongs_to_many

--- app/models/region.rb
+++ app/models/region.rb
@@ -1,5 +1,8 @@
 class Region < ActiveRecord::Base
   has_and_belongs_to_many :accounts
+  has_and_belongs_to_many :countries, :join_table => :regions_countries
+  has_and_belongs_to_many :subgroups, :join_table => :subgroups_regions
+
   
   def self.populate
     # place holder

38ef5a280c0d8a41e7847dbac92648ecd2182387
liwenl <lliu@metrostarsystems.com>
2015-04-13 10:07:24 -0400

    unless connection.table_exists?

--- db/migrate/20150410182334_create_subgroups_regions.rb
+++ db/migrate/20150410182334_create_subgroups_regions.rb
@@ -1,9 +1,11 @@
 class CreateSubgroupsRegions < ActiveRecord::Migration
   def change
+    unless connection.table_exists? 'subgroups_regions'
     create_table :subgroups_regions do |t|
       t.integer :subgroup_id
       t.integer :region_id
       t.timestamp
+    end
     end
   end
 end
--- db/migrate/20150410190725_create_regions_countries.rb
+++ db/migrate/20150410190725_create_regions_countries.rb
@@ -1,9 +1,11 @@
 class CreateRegionsCountries < ActiveRecord::Migration
   def change
+    unless connection.table_exists? 'regions_countries'
     create_table :regions_countries do |t|
       t.integer :region_id
       t.integer :country_id
       t.timestamp
+    end
     end
   end
 end

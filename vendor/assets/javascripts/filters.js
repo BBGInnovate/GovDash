8449b10d39fd54e48c7ca3342eef1649cd86c032
Hayat Noor <hnoor@bbg.gov>
2015-04-13 16:41:20 -0400

    added facebook likes back in

--- vendor/assets/javascripts/filters.js
+++ vendor/assets/javascripts/filters.js
@@ -210,8 +210,8 @@ angular.module('filters', []).
 				var array = [];
 
 				if (socialMediaType === 'fb') {
-				//	array = ['comments', 'story_likes', 'shares', 'page_likes'];
-					array = ['comments', 'story_likes', 'shares'];
+					array = ['comments', 'story_likes', 'shares', 'page_likes'];
+				//	array = ['comments', 'story_likes', 'shares'];
 				} else if (socialMediaType === 'tw') {
 					array = ['retweets', 'mentions', 'favorites', 'followers'];
 				} else if (socialMediaType === 'yt') {

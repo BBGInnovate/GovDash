7224b23cbc7d3182aecb74e614a15da3f3796a8c
liwenl <lliu@metrostarsystems.com>
2015-04-13 14:43:14 -0400

    add_related

--- app/controllers/api/v2/regions_controller.rb
+++ app/controllers/api/v2/regions_controller.rb
@@ -4,18 +4,32 @@ class Api::V2::RegionsController < Api::V2::BaseController
 
   def add_related model_object
     hsh = nil
+    hsh = {:related_region_names=>[], :related_region_names=>[],
+          :related_subgroups=>[]}
+        
     if Region === model_object
       account_ids = AccountsRegion.where(["region_id = ?", model_object.id]).map{|ac| ac.account_id}.uniq
       if !account_ids.empty?
+        # hsh = {:related_region_names=>[], :related_region_names=>[]}
+          
         names = AccountsRegion.where(["account_id in (?)", account_ids]).map{|ac| ac.region.name }
         related_region_names = names.uniq - [model_object.name] 
         if !related_region_names.empty?
-          hsh = {:related_region_names=>[], :related_region_names=>[]}
-          hsh[:related_region_names] = related_region_names
+         hsh[:related_region_names] = related_region_names
           ids = AccountsRegion.where(["account_id in (?)", account_ids]).map{|ac| ac.region.id }
           hsh[:related_region_ids] = ids.uniq - [model_object.id]
         end
-      end 
+      end
+      subgroup_ids = SubgroupsRegion.select("subgroup_id").
+        where(["region_id = ?", model_object.id]).map(&:subgroup_id).uniq
+      
+      Subgroup.where(["id in (?)", subgroup_ids]).to_a.each do |sg|
+        attr = sg.attributes
+        ['created_at','updated_at'].each do |col|
+          attr.delete col
+        end
+        hsh[:related_subgroups] << attr
+      end
     end
     hsh
   end

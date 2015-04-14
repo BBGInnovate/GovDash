caa9aff224996bc39c4f13d127c37d51fb01ec36
liwenl <lliu@metrostarsystems.com>
2015-04-14 14:22:47 -0400

    fix broken code

--- app/controllers/api/v2/regions_controller.rb
+++ app/controllers/api/v2/regions_controller.rb
@@ -3,20 +3,20 @@ class Api::V2::RegionsController < Api::V2::BaseController
   #before_filter :is_analyst?
 
   def add_related model_object
-    hsh = nil
-    hsh = {:related_region_names=>[], :related_region_names=>[],
-          :related_subgroups=>[],
-          :related_countries=>[]}
-        
+    hsh = {:related_region_names=>[], 
+           :related_region_names=>[],
+           :related_subgroups=>[],
+           :related_countries=>[]}       
     if Region === model_object
-      account_ids = AccountsRegion.where(["region_id = ?", model_object.id]).map{|ac| ac.account_id}.uniq
+      account_ids = AccountsRegion.select("account_id").
+                      where(["region_id = ?", model_object.id]).
+                      map(&:account_id).uniq
       if !account_ids.empty?
-        # hsh = {:related_region_names=>[], :related_region_names=>[]}
-          
-        names = AccountsRegion.where(["account_id in (?)", account_ids]).map{|ac| ac.region.name }
-        related_region_names = names.uniq - [model_object.name] 
+        names = AccountsRegion.where(["account_id in (?)", account_ids]).
+           map{|ac| ac.region.name }.uniq
+        related_region_names = names - [model_object.name] 
         if !related_region_names.empty?
-         hsh[:related_region_names] = related_region_names
+          hsh[:related_region_names] = related_region_names
           ids = AccountsRegion.where(["account_id in (?)", account_ids]).map{|ac| ac.region.id }
           hsh[:related_region_ids] = ids.uniq - [model_object.id]
         end
@@ -26,19 +26,22 @@ class Api::V2::RegionsController < Api::V2::BaseController
       
       Subgroup.where(["id in (?)", subgroup_ids]).to_a.each do |sg|
         attr = sg.attributes
-        ['created_at','updated_at'].each do |col|
+        ['created_at','updated_at','is_active'].each do |col|
           attr.delete col
         end
         hsh[:related_subgroups] << attr
       end
       
-      RegionsCountry.select("country_id").
-        where(region_id: model_object.id).
-        map(&:country_id).each | con |
-          country = Country.find con
-          attr = country.attributes
-          hsh[:related_countries] << attr
+      country_ids = RegionsCountry.select("country_id").
+                      where(region_id: model_object.id).
+                      map(&:country_id).uniq
+      country_ids.each do | con |
+        country = Country.find con
+        attr = country.attributes
+        ['is_active','region_id'].each do |n|
+          attr.delete n
         end
+        hsh[:related_countries] << attr
       end
     end
     hsh

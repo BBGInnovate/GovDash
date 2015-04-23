106676fb06bbde2fabac952590b26cec4efc7170
liwenl <lliu@metrostarsystems.com>
2015-04-23 15:02:02 -0400

    optimize get_related

--- app/controllers/api/v2/groups_controller.rb
+++ app/controllers/api/v2/groups_controller.rb
@@ -5,20 +5,14 @@ class Api::V2::GroupsController < Api::V2::BaseController
     #attach related Subgroups
     hsh = nil
     if Group === model_object
-      group_ids = GroupsSubgroups.select("distinct group_id").
-        where(["group_id = ?", model_object.id]).map(&:group_id).to_a
-      if !group_ids.empty?
-        subgroup_ids = GroupsSubgroups.select("distinct subgroup_id").
-          where(["group_id in (?)", group_ids]).map(&:subgroup_id).to_a
-        
-        subgrps = Subgroup.where(["id in (?)", subgroup_ids]).to_a
-        if !subgrps.empty?
-          hsh = {:related_subgroups=>[]}
-          subgrps.each do |sg|
-            hsh[:related_subgroups] = sg.attributes
-          end
+      sql1 = "select distinct subgroup_id from groups_subgroups where group_id = #{model_object.id}"
+      subgrps = Subgroup.where("id in (#{sql1})").to_a
+      if subgrps.count > 0
+        hsh = {:related_subgroups=>[]}
+        subgrps.each do |sg|
+          hsh[:related_subgroups] = sg.attributes
         end
-      end 
+      end
     end
     hsh
   end

d62556be5fced1c7a7817e6a128cdf3a3b885243
liwenl <lliu@metrostarsystems.com>
2015-05-13 07:43:26 -0400

    use helper

--- app/controllers/api/v2/subgroups_controller.rb
+++ app/controllers/api/v2/subgroups_controller.rb
@@ -3,23 +3,19 @@ class Api::V2::SubgroupsController < Api::V2::BaseController
   #attach related Groups output
   def add_related model_object
     if Subgroup === model_object
-      if !@region_hash
-        @region_hash = get_subgroup_region_hash
-      end
-      if !@group_hash
-        @group_hash = get_subgroup_group_hash
-      end
+      @region_array = get_subgroup_region_hash[model_object.id]
+      @group_array = get_subgroup_group_hash[model_object.id]
       hsh = {:related_groups=>[],:related_regions=>[]}
       # sql1 = "select distinct group_id from groups_subgroups where subgroup_id = #{model_object.id}"
       # groups = Group.select("id,name,description,organization_id").where("id in (#{sql1})").to_a
       # groups.to_a.each do |grp|
-      @group_hash[model_object.id].each do |grp|
+      @group_array.each do |grp|
         # attr = grp.attributes
         hsh[:related_groups] << grp
       end
       # sql1 = "select distinct region_id from subgroups_regions where subgroup_id = #{model_object.id}"
       # Region.where("id in (#{sql1})").to_a.each do |reg|
-      @region_hash[model_object.id].each do |reg|
+      @region_array.each do |reg|
         # hsh[:related_regions] << reg.attributes
         hsh[:related_regions] << reg
       end

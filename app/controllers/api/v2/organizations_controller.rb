c063f1f04eeee86871862c9329498a5c4abc1094
liwenl <lliu@metrostarsystems.com>
2015-06-03 13:31:25 -0400

    def add_related obj

--- app/controllers/api/v2/organizations_controller.rb
+++ app/controllers/api/v2/organizations_controller.rb
@@ -3,6 +3,18 @@ class Api::V2::OrganizationsController < Api::V2::BaseController
   #before_filter :is_analyst?
   #before_filter :is_admin?, only: [:new, :create, :edit, :update, :destroy]
 
+  def add_related obj
+    grp_arr = []
+    subgrp_arr = []
+    obj.groups.includes(:subgroups).each do |grp|
+      grp_arr << {id: grp.id, name: grp.name }
+      grp.subgroups.each do |subgrp|
+        subgrp_arr << {id: subgrp.id, name: subgrp.name }
+      end
+    end
+    {:groups=>grp_arr, :subgroups=>subgrp_arr}
+  end
+  
   def __option_for_select
     cond = super
     user = current_user

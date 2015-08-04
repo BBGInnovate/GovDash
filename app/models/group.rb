34807396a420b49384510e02861be7250e175eba
liwenl <lliu@metrostarsystems.com>
2015-08-04 08:18:48 -0400

    add registration confirmation

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,7 @@
 class Group < ActiveRecord::Base
   belongs_to :organization
-  has_many :accounts
+  # has_many :accounts
+  has_and_belongs_to_many :accounts
   has_many :groups_subgroups, class_name: GroupsSubgroups
   has_many :subgroups, :through => :groups_subgroups
 

70e712548cce5a79d29b931f04a37c2fc23e2e66
liwenl <lliu@metrostarsystems.com>
2015-08-04 15:59:33 -0400

    add self.pending_id etc.

--- app/models/subrole.rb
+++ app/models/subrole.rb
@@ -1,3 +1,19 @@
 class Subrole < ActiveRecord::Base
    has_many :users
+
+   def self.pending_id
+     find_by(name: "Pending").id
+   end
+   def self.viewer_id
+     find_by(name: "Viewer").id
+   end
+   def self.group_admin_id
+     find_by(name: "Group Admin").id
+   end
+   def self.organization_admin_id
+     find_by(name: "Organization Admin").id
+   end
+   def self.super_admin_id
+     find_by(name: "Super Admin").id
+   end
 end

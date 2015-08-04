b7770657a689f035a9dc6a2febbded4952eab286
liwenl <lliu@metrostarsystems.com>
2015-08-04 16:02:00 -0400

    #  before_filter :is_service_chief?

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,7 +1,7 @@
 class Admin::BaseController < ApplicationController
   layout "regular"
   before_filter :authenticate_user!
-  before_filter :is_service_chief?
+#  before_filter :is_service_chief?
   
   protected
    

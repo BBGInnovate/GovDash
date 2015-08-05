75b6e646093df3fcf95bea3c8fa413f56ce60995
liwenl <lliu@metrostarsystems.com>
2015-08-05 12:41:12 -0400

    ad is_admin & fix bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -152,21 +152,39 @@ class User < ActiveRecord::Base
     return @hash if !@hash.blank?
     @hash = {:group=>[],:subgroup=>[],:account=>[],
       :organization=>[]}
-    if self.subrole_id && self.subrole_id > 2
-      if self.subrole.name == 'Group Admin' 
-        @hash[:group] = [self.group_id]
-        group = Group.find self.group_id
-        @hash[:account] = group.accounts.map(&:id)
-        @hash[:subgroup]  = GroupsSubgroups.where("group_id = #{self.group_id}").
-                    map(&:subgroup_id)
-        @hash[:organization] = [self.organization.id]
-      elsif self.subrole.name == 'Organization Admin' &&
-        self.organization && self.organization.groups.size > 1
-        @hash[:account] = self.organization.accounts.map(&:id)
-        @hash[:group] = self.organization.groups.map(&:id)
-        @hash[:subgroup] = GroupsSubgroups.where("group_id in (#{hash[:group_ids].join(',')})").
+    if self.is_admin?
+      @hash[:account] = []
+      @hash[:organization]=[]
+      @hash[:group] = []
+      @hash[:subgroup]=[]
+      Organization.all.each do |org|
+        @hash[:organization] << org.id
+        @hash[:account] << org.accounts.map(&:id)
+        group = org.groups.map(&:id)
+        @hash[:group] << group
+        @hash[:subgroup] << GroupsSubgroups.where(["group_id in (?)", group]).
                       map(&:subgroup_id)
-        @hash[:organization] = [self.organization.id]
+      end
+      @hash[:account].flatten!
+      @hash[:group].flatten!
+      @hash[:subgroup].flatten!
+    else
+      if self.subrole_id && self.subrole_id > 2
+        if self.subrole.name == 'Group Admin' 
+          @hash[:group] = [self.group_id]
+          group = Group.find self.group_id
+          @hash[:account] = group.accounts.map(&:id)
+          @hash[:subgroup]  = GroupsSubgroups.where("group_id = #{self.group_id}").
+                    map(&:subgroup_id)
+          @hash[:organization] = [self.organization.id]
+        elsif self.subrole.name == 'Organization Admin' &&
+          self.organization && self.organization.groups.size > 1
+          @hash[:account] = self.organization.accounts.map(&:id)
+          @hash[:group] = self.organization.groups.map(&:id)
+          @hash[:subgroup] = GroupsSubgroups.where(["group_id in (?)", @hash[:group]]).
+                      map(&:subgroup_id)
+          @hash[:organization] = [self.organization.id]
+        end
       end
     end
     @hash

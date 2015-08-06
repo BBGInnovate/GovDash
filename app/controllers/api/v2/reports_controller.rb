581c6fae46050f6f5e972d2e941007defef16a88
liwenl <lliu@metrostarsystems.com>
2015-08-06 15:52:01 -0400

    remove page_likes from trend engagement data

--- app/controllers/api/v2/reports_controller.rb
+++ app/controllers/api/v2/reports_controller.rb
@@ -46,7 +46,7 @@ class Api::V2::ReportsController < Api::V2::BaseController
     #current_user.roles.map(&:organization_id)
     unless current_user.is_admin?
       # come from /api/reports
-      if params[:options]
+      if params[:options] && !params[:options][:organization_ids]
         params[:options][:organization_ids] = current_user.roles.map(&:organization_id)
       end
     end
@@ -57,13 +57,15 @@ class Api::V2::ReportsController < Api::V2::BaseController
   # replace get_facebooks etc.
   #
   def get_reports source = nil
+    # source = 'facebook'
     if source && source.downcase != "all"
-      medias = ["#{source.upcase}Account"]
+      medias = ["#{source.titleize}Account"]
     else
       medias = accounts.map(&:media_type_name).uniq
     end
     medias.each do |media_type_name| 
       names = account_names_for media_type_name.to_s
+      # logger.debug " Names : #{names}"
       if !names.empty?
         stat = get_stat_class(media_type_name)
         if stat
@@ -127,30 +129,51 @@ class Api::V2::ReportsController < Api::V2::BaseController
     get_options
   end
 
-  def get_options
-    logger.debug " BBB #{params[:options].inspect}"
-    @options = params[:options] || {}
-    end_date = @options[:end_date]
+  def adjust_start_end_dates
+    end_date = @options[:end_date] ? @options[:end_date] : (Time.zone.now-1.days).strftime('%Y-%m-%d') 
+    @options[:end_date] = parse_date(end_date).end_of_day
     start_date = @options[:start_date]
-    @options[:end_date] = !!end_date ? end_date : (Time.zone.now-1.days).strftime('%Y-%m-%d') 
+    if !start_date 
+      begin
+        start_date = @options[:end_date] - instance_eval(@options[:period]) 
+      rescue
+        start_date = @options[:end_date] - 1.week
+      end
+    end
     @options[:start_date] = parse_date(start_date).beginning_of_day
-    @options[:end_date] = parse_date(@options[:end_date]).end_of_day
-    # overwrite period parameter is start_date exists
-    diff = @options[:end_date] - @options[:start_date]
-    @total_days = diff.to_i/(3600*24) + 1 
-    if @total_days > 27
+    total_days = (@options[:end_date] - @options[:start_date]).to_i/(3600*24) + 1 
+    if total_days > 27
       if !@options[:start_date].sunday?
         @options[:start_date] = @options[:start_date].end_of_week.beginning_of_day
       end
       if !@options[:end_date].saturday?
-        @options[:end_date]=(@options[:end_date].end_of_week-8.day).end_of_day
+        day = @options[:end_date]
+        (1..6).each do | i |
+          day -= 1.day
+          if day.wday == 6
+            break
+          end
+        end
+        @options[:end_date]=day.end_of_day
+        # logger.debug "  AAA3 #{@options[:start_date]} - #{@options[:end_date]} " 
       end
+    end
+    total_days
+  end
+
+  def get_options
+    @options = params[:options] || {}
+    total_days = adjust_start_end_dates
+    # overwrite period parameter
+    if total_days > 27
       net_days = ((@options[:end_date] - @options[:start_date])/(3600*24)).ceil
-      logger.debug "  NNN net_days=#{net_days}"
+      # logger.debug "  NNN net_days=#{net_days}"
       @options[:period] = net_days.days
-      @options[:trend] = 'monthly'
+      # sum data for each week
+      @options[:trend] = 'weekly'
     else
-      @options[:period] = @total_days.days
+      @options[:period] = total_days.days
+      # sum data for each day
       @options[:trend] = 'daily'
     end
     @options[:account_ids] = Account.get_account_ids @options
@@ -169,154 +192,5 @@ class Api::V2::ReportsController < Api::V2::BaseController
     # do nothing
   end
 end
-=begin
-  def convert collection
-    arrs = []
-    collection.each do | elem |
-      begin
-        hsh = {}
-        hsh[:name] = elem[:name]
-        hsh[:countries] = elem[:countries]
-        hsh[:values] = []
-        next if !elem[:values]
-        elem[:values].each do | value |
-          if Array === value
-            value.each do |v|
-              attributes = v.attributes rescue v
-              attributes.delete('id') if attributes.respond_to?(:delete)
-              hsh[:values] << attributes
-            end
-          else
-            attributes = value.attributes rescue value
-            attributes.delete('id') if attributes.respond_to?(:delete)
-            hsh[:values] << attributes
-          end
-        end 
-        arrs << hsh
-      rescue Exception=>error
-        logger.error error.message
-        error.backtrace.each do |m|
-          logger.error "#{m}"
-        end
-      end
-    end
-    arrs
-  end
-  def select_trend stat
-    return unless @options[:trend]
-    if @options[:trend].match(/week/i)
-      stat.select_trend_by_day
-    elsif @options[:trend].match(/month/i)
-      stat.select_trend_by_week
-    elsif @options[:trend].match(/year/i)
-      stat.select_trend_by_month
-    else
-      stat.select_trend_by_day
-    end
-  end
-    def get_facebooks
-    begin
-      names = account_names_for "FacebookAccount"
-    rescue Exception=>error
-      logger.error error.message
-      # error.backtrace.each do |m|
-      #  logger.error "#{m}"
-      #end
-      return nil
-    end
-    
-    if names.empty?
-      return nil
-    end
-    begin
-      stat = FbStat.new(@options)
-      sel = stat.select_by
-      if !sel
-        return nil
-      else
-        @report = {:facebook => {
-             :accounts=> accounts_for("FacebookAccount").to_h,
-             :countries => related_countries_for("FacebookAccount").to_h,
-             :regions => related_regiinitons_for("FacebookAccount").to_h
-           }}
-        @report[:facebook][:values] = sel
-        acc = stat.select_accounts
-        @report[:facebook][:values].merge! acc if acc
-        @report
-      end
-    rescue Exception=>error
-      logger.error error.message
-      error.backtrace.each do |m|
-        logger.error "#{m}"
-      end
-      nil
-    end
-  end
-  
-  def get_twitters
-    names = tw_account_names
-    if names.empty?
-      return nil
-    end
-    begin
-      stat = TwStat.new @options
-      sel = stat.select_by
-      if !sel
-        return nil
-      else
-        @report = {:twitter => {
-                 :accounts=> tw_accounts.to_h,
-                 :countries => tw_related_countries.to_h,
-                 :regions => tw_related_regions.to_h
-                }}
-        @report[:twitter][:values] = sel
-        acc = stat.select_accounts
-        @report[:twitter][:values].merge! acc if acc
-        @report
-      end
-    rescue Exception=>error
-      logger.error error.message
-      error.backtrace.each do |m|
-        logger.error "#{m}"
-      end
-      # hsh
-      nil
-    end
-  end
-  def get_youtubes
-    begin
-      names = yt_account_names
-    rescue Exception=>error
-      logger.error error.message
-      return nil
-    end
-    
-    if names.empty?
-      return nil
-    end
-    begin
-      stat = YtStat.new(@options)
-      sel = stat.select_by
-      if !sel
-        return nil
-      else
-        @report = {:youtube => {
-             :accounts=> yt_accounts.to_h,
-             :countries => yt_related_countries.to_h,
-             :regions => yt_related_regions.to_h
-           }}
-        @report[:youtube][:values] = sel
-        acc = stat.select_accounts
-        @report[:youtube][:values].merge! acc if acc
-        @report
-      end
-    rescue Exception=>error
-      logger.error error.message
-      error.backtrace.each do |m|
-        logger.error "#{m}"
-      end
-      nil
-    end
-  end
-=end
+
 

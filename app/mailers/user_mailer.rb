3129b8e820c8d06a532bb8ae304e599a2605e3c2
liwenl <lliu@metrostarsystems.com>
2015-10-07 11:16:04 -0400

    add comments

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,12 +3,12 @@ class UserMailer < ActionMailer::Base
   before_action :set_headers
 
   # UserMailer.alarm_email(['liw@bbg.gov'], 'Testing AWS Email Service').deliver_now!
- 
+  # for data not inserted for a period
   def alarm_email(email, message)
       @message = message
       mail(to: email, subject: @message)
   end
-
+  # this is for data missing for certain date
   def alert(email, subject, message)
       @message = message
       mail(to: email, subject: subject)

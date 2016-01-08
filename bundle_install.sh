a1ade49300072682dc2cc50eff5355a4670c7d64
liwenl <lliu@metrostarsystems.com>
2016-01-08 11:57:20 -0500

    for new govdash server

--- bundle_install.sh
+++ bundle_install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-rake=/usr/local/bin/rake
-bundle=/usr/local/bin/bundle
+source /home/uberdash/.rvm/scripts/rvm
+rake=/home/uberdash/.rvm/gems/ruby-2.2.1/bin/rake
+bundle=/home/uberdash/.rvm/gems/ruby-2.2.1/bin/bundle
 
 root=/srv/www/socialdash_app
 current_path="${root}/current"
@@ -9,11 +10,13 @@ shared_path="${root}/shared"
 release_path="$root/releases"
 name=$1
 
-export PATH=/usr/local/bin:$PATH
+# export PATH=/usr/local/bin:$PATH
 export RAILS_ENV=production
 # cd $release_path/$name
 
-$bundle exec bundle install --path=/home/deploy/.bundler/socialdash_app --without=test development
+# $bundle exec bundle install --path=/home/deploy/.bundler/socialdash_app --without=test development
+$bundle install --without=test development
 ${rake} db:migrate
+ln -s $release_path $current_path
 touch tmp/restart.txt
     

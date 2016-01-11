24af37bb979e5c86043992cfb4b24c6510ad644f
liwenl <lliu@metrostarsystems.com>
2016-01-11 09:55:49 -0500

    retry get_app_access_token

--- app/models/app_token.rb
+++ app/models/app_token.rb
@@ -2,19 +2,28 @@ class AppToken < ActiveRecord::Base
 
   self.table_name = "app_tokens"
 
-  def get_access_token
+  def get_access_token times=1
+    return nil if times > 3
     if self.page_access_token && 
        self.updated_at > 55.minutes.ago
       self.page_access_token
     else
       callback_url = canvas_url
       @oauth = Koala::Facebook::OAuth.new(client_id, client_secret, callback_url)
-      access_token = @oauth.get_app_access_token
-      self.update_attribute :page_access_token, access_token
-      access_token
+      begin
+        access_token = @oauth.get_app_access_token
+        self.update_attribute :page_access_token, access_token
+        access_token
+      rescue Exception=>ex
+        times = times + 1
+        logger.error ex.message
+        logger.info "  retry #{times} times in 5 seconds"
+        sleep 5
+        access_token = get_access_token times
+      end
     end 
   end
-
+  
   def exchange_page_access_token(access_token)
     if !access_token
       raise "  AppToken#exchange_page_access_token access_token is null"

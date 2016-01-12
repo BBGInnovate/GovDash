ad2d20017787430052727960e6eaae3312c9af79
liwenl <lliu@metrostarsystems.com>
2016-01-12 16:15:09 -0500

    added

--- bundle_install.sh
+++ bundle_install.sh
@@ -9,24 +9,8 @@ current_path="${root}/current"
 shared_path="${root}/shared"
 release_path="$root/releases"
 
-configs="config/database.yml config/email.yml config/facebook.yml config/rabbit.yml config/twitter.yml \
-  config/youtube.yml config/s3.yml config/sitecatalyst.yml \
-  log"
-secret_token="config/secret_token.rb"
-
-for j in $configs
-do 
-  ln -fs ${shared_path}/system public/system; \
-  ln -fs ${shared_path}/$secret_token config/initializers/secret_token.rb
-  ln -fs ${shared_path}/$j $j
-done
-
 export RAILS_ENV=production
 # $bundle exec bundle install --path=/home/deploy/.bundler/socialdash_app --without=test development
 bundle install --without=test development
 rake db:migrate
-rm $current_path
-ln -fs $curr $current_path
-cd $current_path
 touch tmp/restart.txt
-

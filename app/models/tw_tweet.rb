282354ec36980305b21e78b307b09f5fa1d3a879
liwenl <lliu@metrostarsystems.com>
2016-01-26 13:10:14 -0500

    add def self.delete_duplicates

--- app/models/tw_tweet.rb
+++ app/models/tw_tweet.rb
@@ -11,4 +11,12 @@ class TwTweet < ActiveRecord::Base
   # to clear cache of method create_or_update
   handle_asynchronously :sync_redshift, :run_at => Proc.new { 5.seconds.from_now }
   
+  def self.delete_duplicates
+    dups = TwTweet.select("account_id, tweet_id, count(*) as cont").
+       group([:account_id,:tweet_id]).having("cont > 1").to_a  
+    dups.each do | du |
+      sql = "delete from tw_tweets where tweet_id = #{du.tweet_id} limit #{du.cont-1}"
+      ActiveRecord::Base.connection.send(:delete_sql, sql)
+    end
+  end
 end
